21445aa1819e396865075115ccff3bd264febe2a
anna <annabyabe@gmail.com>
2025-06-24 14:34:49 -0300

    Exercise 2.5

--- 2_Communicating-with-Server/courseproject/src/components/Course.jsx
+++ 2_Communicating-with-Server/courseproject/src/components/Course.jsx
@@ -1,5 +1,20 @@
-import Contents from './Contents'
-import Summary from './Summary'
+const Contents = ({content}) => {
+    return (
+        content.map((c, i) => 
+            <p key={i}>
+                {c.name} {c.exercises}
+            </p>
+        )
+    )
+}
+
+const Summary = ({content}) => {
+    const exercises = content.map(c => c.exercises)
+    const total = exercises.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    return(
+        <p>Total of {total} exercises</p>
+    )
+}
 
 const Course = ({course}) => {
     return (
--- 2_Communicating-with-Server/courseproject/src/components/Summary.jsx
+++
@@ -1,9 +0,0 @@
-const Summary = ({content}) => {
-    const exercises = content.map(c => c.exercises)
-    const total = exercises.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-    return(
-        <p>Total of {total} exercises</p>
-    )
-}
-
-export default Summary
