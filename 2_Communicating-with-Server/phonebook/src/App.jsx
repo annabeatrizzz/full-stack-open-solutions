731681393bf9494df33b5d08d7f6e46adeab0bfa
anna <annabyabe@gmail.com>
2025-08-17 11:10:26 -0300

    Exercise 2.9

--- 2_Communicating-with-Server/phonebook/src/App.jsx
+++ 2_Communicating-with-Server/phonebook/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '123-456-789' }
+    { name: 'Arto Hellas', phone: '123-456-789', id: 1},
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ]) 
   
   const defaultName = 'New name'
   const defaultNumber = '999-999-999'
   const [newName, setNewName] = useState(defaultName)
+  const [contactName, setContactName] = useState('')
   const [newNumber, setNewNumber] = useState(defaultNumber)
 
   const handleNewName = (event) => {
@@ -43,10 +47,25 @@ const App = () => {
     setNewNumber(defaultNumber)
   }
 
+  const handleSearch = (event) => {
+    setContactName(event.target.value)
+    console.log(contactName)
+
+    const personsFiltered = persons.filter(p => p.name.toLowerCase().includes(contactName.toLowerCase()))
+    setPersons(personsFiltered)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <h2>Search</h2>
+      <div>
+          Filter contacts lists with: <input value={contactName} onChange={handleSearch}/>
+      </div>
+      
       <form onSubmit={handleForm}> 
+        <h2>New contact</h2>
         <div>
           Name: <input value={newName} onChange={handleNewName}/>
         </div>
