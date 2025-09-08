3711e2414fb057358d77f9fde07659e70841d4d7
anna <annabyabe@gmail.com>
2025-09-08 15:27:06 -0300

    Exercise 3.10

--- 3_Programming-a-Server/phonebook/index.js
+++ 3_Programming-a-Server/phonebook/index.js
@@ -106,6 +106,7 @@ app.get('/api/info', (request, response) => {
     `)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
