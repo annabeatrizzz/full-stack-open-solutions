42f990ecbd1e05cae16889029fde22c617f6d114
anna <annabyabe@gmail.com>
2025-09-19 17:04:12 -0300

    Exercise 4.4

--- 4_Backend-and-Testing/blog/utils/list_helper.js
+++ 4_Backend-and-Testing/blog/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blog) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return blog.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
