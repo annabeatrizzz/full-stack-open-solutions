15e9d89b29d0dbd8844e066b6ef55a07e3e102e2
anna <annabyabe@gmail.com>
2025-09-21 04:32:05 -0300

    Exercise 4.10

--- 4_Backend-and-Testing/blog/test/blog_api.test.js
+++ 4_Backend-and-Testing/blog/test/blog_api.test.js
@@ -34,6 +34,24 @@ test('blogs unique identifier is named id', async () => {
   assert.strictEqual(response.body[0].id, '5a422a851b54a676234d17f7')
 })
 
+test('new blogs are correctly created', async () => {
+  await api
+    .post('/api/blogs')
+    .send(listHelper.listWithManyBlogs[2])
+    .expect(201)
+  
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    
+  assert.strictEqual(response.body.length, 3)
+  assert.strictEqual(response.body[2].title, listHelper.listWithManyBlogs[2].title)
+  assert.strictEqual(response.body[2].author, listHelper.listWithManyBlogs[2].author)
+  assert.strictEqual(response.body[2].url, listHelper.listWithManyBlogs[2].url)
+  assert.strictEqual(response.body[2].likes, listHelper.listWithManyBlogs[2].likes)
+})
+
 
 after(async () => {
   await mongoose.connection.close()
