5b809ccdf87940e9d9931a3d47347cdee6c9cc76
anna <annabyabe@gmail.com>
2025-10-15 16:35:20 -0300

    Exercise 6.14

--- 6_State-Management/redux-anecdotes/src/App.jsx
+++ 6_State-Management/redux-anecdotes/src/App.jsx
@@ -1,13 +1,22 @@
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
-import store from './store'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
+import { setAnecdote } from './reducers/anecdoteReducer'
+import anecdoteService from './service/anecdote'
+
 const App = () => {
+  const dispatch = useDispatch()
   const message = useSelector(state => state.message)
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdote => dispatch(setAnecdote(anecdote)))
+  }, [dispatch])
 
   return (
     <div>
