af88f634cd6c05703d078b77524b3cfe98e921b9
anna <annabyabe@gmail.com>
2025-10-15 16:08:32 -0300

    Exercise 6.12

--- 6_State-Management/redux-anecdotes/src/App.jsx
+++ 6_State-Management/redux-anecdotes/src/App.jsx
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux'
+
+import store from './store'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
+  const message = useSelector(state => state.message)
 
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification content={message}></Notification>
       <Filter></Filter> 
       <AnecdoteList></AnecdoteList>   
       <AnecdoteForm></AnecdoteForm>
--- 6_State-Management/redux-anecdotes/src/store.js
+++ 6_State-Management/redux-anecdotes/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import filterReducer from './reducers/filterReducer'
+import messageReducer from './reducers/messageReducer'
 import anecdoteReducer from './reducers/anecdoteReducer'
 
 const store = configureStore({
   reducer: {
     anecdote: anecdoteReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    message: messageReducer
   }
 })
 
