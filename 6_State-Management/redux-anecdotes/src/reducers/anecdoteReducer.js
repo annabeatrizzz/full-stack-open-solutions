63c9a57f677d45dc468a292caf91b55b03e288dc
anna <annabyabe@gmail.com>
2025-10-15 16:39:16 -0300

    Exercise 6.15

--- 6_State-Management/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ 6_State-Management/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -15,12 +15,7 @@ const anecdoteSlice = createSlice({
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        votes: 0,
-        id: getId(),
-      })
+      state.push(action.payload)
     },
     createVote(state, action) {
       const id = action.payload
