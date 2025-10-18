af3603b1c189185843ba9f2eb7be0275a04ec7f8
anna <annabyabe@gmail.com>
2025-10-18 12:54:03 -0300

    Exercise 7.7

--- 7_ReactRouter-CustomHooks-Styling/country-hook/src/App.jsx
+++ 7_ReactRouter-CustomHooks-Styling/country-hook/src/App.jsx
@@ -18,7 +18,24 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) {
+      setCountry(null)
+      return
+    }
+
+    const fetchCountry = async () => {      
+      try {
+        const response = await axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${(name)}`)
+        setCountry({found: true, data: response.data})
+        console.log(response.data.name.common)
+      } catch {
+        setCountry({found: false})
+      }
+    }
+
+    fetchCountry()
+  }, [name])
 
   return country
 }
@@ -38,7 +55,7 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
       <div>population {country.data.population}</div> 
       <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
