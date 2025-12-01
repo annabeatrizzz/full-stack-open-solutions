7b031952e713b810473ffb07b12f07392fb3dd08
anna <annabyabe@gmail.com>
2025-12-01 18:44:54 -0300

    Exercise 10.2 - had to make some modifications to eslint config file

---
+++ 10_ReactNative/rate-repository-app/eslint.config.js
@@ -0,0 +1,36 @@
+import babelParser from "@babel/eslint-parser";
+import reactPlugin from "eslint-plugin-react";
+import reactNativePlugin from "eslint-plugin-react-native";
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-native": reactNativePlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
