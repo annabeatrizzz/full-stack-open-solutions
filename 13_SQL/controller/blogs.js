28c6d781bb317c56cbf22a107dfb6535b32c9463
annabeatrizzz <annabyabe@gmail.com>
2026-01-20 16:24:47 -0300

    Exercise 13.5

---
+++ 13_SQL/controller/blogs.js
@@ -0,0 +1,49 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const noteFinder = async (req, res, next) => {
+  req.note = await Note.findByPk(req.params.id)
+  next()
+}
+
+router.get('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    res.json(req.note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    await req.note.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    req.note.important = req.body.important
+    await req.note.save()
+    res.json(req.note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+module.exports = router
